7804ea2e60b85acdd671edbb32073676e1a2b75d
KathaSu <suhr.katharina@gmail.com>
2017-05-10 21:42:40 +0200

    Hauptbildschirm angepasst

--- app/src/main/java/com/momole/de/projektmomole/MainActivity.java
+++ app/src/main/java/com/momole/de/projektmomole/MainActivity.java
@@ -28,8 +28,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent eingabe = new Intent (MainActivity.this, EingabeActivity.class);
+                startActivity(eingabe);
             }
         });
 
