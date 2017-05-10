c2b2bee58640856a1c04ce912d2da808313d3983
manjit <manjit.kaur@stud.hs-hannover.de>
2017-05-10 20:23:14 +0200

    DBHeper

--- app/src/main/java/com/momole/de/projektmomole/database/DatabaseHelper.java
+++ app/src/main/java/com/momole/de/projektmomole/database/DatabaseHelper.java
@@ -6,5 +6,30 @@ package com.momole.de.projektmomole.database;
 
 public class DatabaseHelper {
 
+    private static final String DATABASE_NAME = "projektmomole.db";
+    private static final int DATABASE_VERSION = 1;
+    private static DatabaseHelper instance;
+
+    private DatabaseHelper(Context context, SQLiteDatabase.CursorFactory factory){
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
+    }
+
+    public static DatabaseHelper getInstance(Context context){
+        if (instance == null){
+            instance = new DatabaseHelper(context, null);
+        }
+        return instance;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase database){
+        MomoleDAO.getInstance(null).onCreate(database);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion){
+        MomoleDAO.getInstance(null).onUpgrade(database, oldVersion, newVersion);
+    }
+
 
 }
