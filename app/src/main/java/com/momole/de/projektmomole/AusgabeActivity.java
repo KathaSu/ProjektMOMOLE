a8effad0330908b08e3283750664f4a017ae55df
KathaSu <suhr.katharina@gmail.com>
2017-05-16 11:59:34 +0200

    ausgabe gemerged

--- app/src/main/java/com/momole/de/projektmomole/AusgabeActivity.java
+++ app/src/main/java/com/momole/de/projektmomole/AusgabeActivity.java
@@ -2,6 +2,7 @@ package com.momole.de.projektmomole;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AusgabeActivity extends AppCompatActivity {
 
