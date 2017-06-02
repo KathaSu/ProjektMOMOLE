ce7fc48867ed2db1f50d9fa12730a444153ef3b7
manjit <manjit.kaur@stud.hs-hannover.de>
2017-06-02 23:54:00 +0200

    Eingabe verkn mit DB

--- app/src/main/java/com/momole/de/projektmomole/EingabeActivity.java
+++ app/src/main/java/com/momole/de/projektmomole/EingabeActivity.java
@@ -37,6 +37,7 @@ public class EingabeActivity extends AppCompatActivity {
         b_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+            }
         });
         }
     }
