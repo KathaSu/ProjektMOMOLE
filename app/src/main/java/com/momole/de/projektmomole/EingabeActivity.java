4932b13fb6863642f15c46cc1d2868ae3c2aa8b3
KathaSu <suhr.katharina@gmail.com>
2017-06-03 13:26:56 +0200

    datenbank anbinden

--- app/src/main/java/com/momole/de/projektmomole/EingabeActivity.java
+++ app/src/main/java/com/momole/de/projektmomole/EingabeActivity.java
@@ -13,58 +13,67 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
-public class EingabeActivity extends AppCompatActivity {
+import static android.text.format.DateFormat.is24HourFormat;
 
-    Button b_save;
+public class EingabeActivity extends AppCompatActivity implements View.OnClickListener{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_eingabe2);
-        b_save = (Button) findViewById(R.id.b_save);
 
-        b_save.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-            }
-        });
-    }
-    findViewById(R.id.b_saveButton).setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            String food = ((TextView) findViewById(R.id.momoleInputfood)).getText().toString();
-            String comp = ((TextView) findViewById(R.id.momoleInputcomp)).getText().toString();
-            try {
-                Momole momole = new Momole();
-                momole.setFood(food);
-                momole.setComp(comp);
-                momole.setDate(date);
-                momole.setTime(System.currentTimeMillis());
-                MomoleDAO.getInstance(EingabeActivity.this).addMomole(momole);
-            }
 
-/*
     Button btnTime, btnDate;
     TextView tvTime, tvDate;
 
     TimePickerDialog timePickerDialog;
     DatePickerDialog datePickerDialog;
 
-    Calendar calender = Calendar.getInstance();
+    Calendar calender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_eingabe2);
 
-        btnTime = (Button) findViewById(R.id.button);
+        btnTime = (Button) findViewById(R.id.btn_zeiteingabe);
         btnTime.setOnClickListener(this);
-        btnDate = (Button) findViewById(R.id.button2);
+        btnDate = (Button) findViewById(R.id.btn_datumeingabe);
         btnDate.setOnClickListener(this);
 
-        tvTime = (TextView) findViewById(R.id.textView3);
-        tvDate = (TextView) findViewById(R.id.textView5);
+        tvTime = (TextView) findViewById(R.id.zeitausgabe);
+        tvDate = (TextView) findViewById(R.id.datumsausgabe);
+
+        findViewById(R.id.btn_speichern).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String lebensmittel = ((TextView) findViewById(R.id.lebensmittel)).getText().toString();
+                String beschwerde = ((TextView) findViewById(R.id.beschwerden)).getText().toString();
+                String date = ((TextView) findViewById(R.id.datumsausgabe)).getText().toString();
+                String time = ((TextView) findViewById(R.id.zeitausgabe)).getText().toString();
+
+                if (date.length() > 0) {
+                    try {
+                        Momole payment = new Payment();
+                        payment.setAmount(paid);
+                        payment.setCategory(cat);
+                        payment.setTime(System.currentTimeMillis());
+                        PaymentDAO.getInstance(PaymentActivity.this).addPayment(payment);
+                        Toast.makeText(PaymentActivity.this, R.string.save_payment_message, Toast.LENGTH_SHORT).show();
+                        finish();
+                    } catch (NumberFormatException e) {
+                        Toast.makeText(PaymentActivity.this, R.string.amount_missing, Toast.LENGTH_LONG).show();
+                    }
+                } else {
+
+                    Toast.makeText(PaymentActivity.this,
+                            R.string.amount_missing, Toast.LENGTH_LONG).show();
+
+                }
+            }
+        }
+
+
+
+
     }
 
     @Override
@@ -73,8 +82,8 @@ public class EingabeActivity extends AppCompatActivity {
         calender = Calendar.getInstance();
 
         switch (v.getId()){
-            
-            case R.id.button: {
+
+            case R.id.btn_zeiteingabe: {
 
                 timePickerDialog = new TimePickerDialog(EingabeActivity.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
@@ -84,19 +93,16 @@ public class EingabeActivity extends AppCompatActivity {
                         timeCalendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
                         timeCalendar.set(Calendar.MINUTE, minute);
 
-                        String timestring = DateUtils.formatDateTime(EingabeActivity.this, timeCalendar.getTimeInMillis(), DateUtils.FORMAT_SHOW_TIME);
-                        tvTime.setText("Uhrzeit: " + timestring);
-
-
+                        String timestring = hourOfDay + ":" + minute;
+                        tvTime.setText(timestring);
                     }
-                },calender.get(calender. HOUR_OF_DAY ), calender.get(calender.MINUTE), android.text.format.DateFormat.is24HourFormat(EingabeActivity.this));
-
+                },calender.get(calender.HOUR_OF_DAY), calender.get(calender.MINUTE), is24HourFormat(this));
 
                 timePickerDialog.show();
-
                 break;
             }
-            case R.id.button2: {
+
+            case R.id.btn_datumeingabe: {
 
                 datePickerDialog = new DatePickerDialog(EingabeActivity.this, new DatePickerDialog.OnDateSetListener() {
                     @Override
@@ -107,8 +113,8 @@ public class EingabeActivity extends AppCompatActivity {
                         dateCalendar.set(calender.MONTH, month);
                         dateCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
 
-                        String dateString = DateUtils.formatDateTime(EingabeActivity.this, dateCalendar.getTimeInMillis(), DateUtils.FORMAT_SHOW_DATE);
-                        tvDate.setText("Datum: " + dateString);
+                        String dateString = dayOfMonth + "-" + month + "-" + year;
+                        tvDate.setText(dateString);
 
                     }
                 }, calender.get(Calendar.YEAR), calender.get(Calendar.MONTH), calender.get(Calendar.DAY_OF_MONTH));
@@ -118,6 +124,6 @@ public class EingabeActivity extends AppCompatActivity {
             }
         }
     }
-    */
+
 }
 
